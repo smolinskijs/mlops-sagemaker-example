5c6b3704cc381d4ec4db6eb3be2a36da038da4fb
Jacek Smolinski <jacek.smolinski@xperi.com>
2024-04-12 11:37:54 +0200

    Switching back to t3

--- pipelines/abalone/pipeline.py
+++ pipelines/abalone/pipeline.py
@@ -128,7 +128,7 @@ def get_pipeline(
     model_package_group_name="AbalonePackageGroup",
     pipeline_name="AbalonePipeline",
     base_job_prefix="Abalone",
-    processing_instance_type="ml.m5.xlarge",
+    processing_instance_type="ml.t3.medium",
     training_instance_type="ml.m5.xlarge",
 ):
     """Gets a SageMaker ML Pipeline instance working with on abalone data.
@@ -286,8 +286,8 @@ def get_pipeline(
     step_args = model.register(
         content_types=["text/csv"],
         response_types=["text/csv"],
-        inference_instances=["ml.t2.medium", "m4.xlarge"],
-        transform_instances=["m4.xlarge"],
+        inference_instances=["ml.t2.medium", "ml.m4.xlarge"],
+        transform_instances=["ml.m4.xlarge"],
         model_package_group_name=model_package_group_name,
         approval_status=model_approval_status,
         model_metrics=model_metrics,
